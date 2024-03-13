b36551524ce6df6191e318fa71458855713fc59b
James Anderson <james.anderson98@outlook.com>
2024-03-13 13:18:12 +0000

    Import page-load.js. Add logic to clear #content div when switching tabs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import './style.css';
+import { buildPage } from './page-load';
+
+buildPage();
 console.log('Hello webpack!');
+
+// clear #content div
+const SwitchTabs = () => {
+    const content = document.getElementById('content');
+    content.innerHTML = '';
+};
+
+// get nav buttons
+const homeBtn = document.getElementById('home');
+const menuBtn = document.getElementById('menu');
+const aboutBtn = document.getElementById('about');
+
+homeBtn.addEventListener('click', SwitchTabs);
+menuBtn.addEventListener('click', SwitchTabs);
+aboutBtn.addEventListener('click', SwitchTabs);
+
+
